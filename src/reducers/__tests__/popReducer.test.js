57212fdcf71d67dff31760a8c638cab2194f30bf
Stuart Lee <stuart.lee@drpgroup.com>
2018-01-24 13:47:01 +0000

    linting the test

--- src/reducers/__tests__/popReducer.test.js
+++ src/reducers/__tests__/popReducer.test.js
@@ -4,11 +4,11 @@ import reducer from '../popReducer'
 describe('popReducer', () => {
   const inputState = Object.freeze({
     index: 1,
-    routes: [{ key: 'route0'}, { key: 'route1'}]
+    routes: [{ key: 'route0' }, { key: 'route1' }]
   })
   const outputState = {
     index: 0,
-    routes: [{ key: 'route0'}]
+    routes: [{ key: 'route0' }]
   }
   it('pops a route off the stack', () => {
     expect(reducer(inputState)).toEqual(outputState)
