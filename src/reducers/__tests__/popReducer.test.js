e0e14c154435268ab4e84681f687fab6bc9a1498
Stuart Lee <stuart.lee@drpgroup.com>
2018-01-24 13:44:44 +0000

    added a first test and got the test config working

---
+++ src/reducers/__tests__/popReducer.test.js
@@ -0,0 +1,16 @@
+/* eslint-env jest */
+import reducer from '../popReducer'
+
+describe('popReducer', () => {
+  const inputState = Object.freeze({
+    index: 1,
+    routes: [{ key: 'route0'}, { key: 'route1'}]
+  })
+  const outputState = {
+    index: 0,
+    routes: [{ key: 'route0'}]
+  }
+  it('pops a route off the stack', () => {
+    expect(reducer(inputState)).toEqual(outputState)
+  })
+})
