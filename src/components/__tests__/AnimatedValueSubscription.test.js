4a9adac8522e1ad0e19042568bdd1617915d14d7
Stuart Lee <stuart.lee@drpgroup.com>
2018-01-25 11:42:10 +0000

    added test for animatedvaluesubscription

---
+++ src/components/__tests__/AnimatedValueSubscription.test.js
@@ -0,0 +1,25 @@
+/* eslint-env jest */
+import AnimatedValueSubscription from '../AnimatedValueSubscription'
+
+describe('AnimatedValueSubscription', () => {
+  it('adds listener', () => {
+    const mockAnimatedValue = {
+      addListener: jest.fn(),
+      removeListener: jest.fn()
+    }
+    const mockCallback = jest.fn()
+    const subscription = new AnimatedValueSubscription(mockAnimatedValue, mockCallback)
+    expect(mockAnimatedValue.addListener).toBeCalledWith(mockCallback)
+  })
+
+  it('removes listener', () => {
+    const mockAnimatedValue = {
+      addListener: jest.fn(),
+      removeListener: jest.fn()
+    }
+    const mockCallback = jest.fn()
+    const subscription = new AnimatedValueSubscription(mockAnimatedValue, mockCallback)
+    subscription.remove()
+    expect(mockAnimatedValue.removeListener).toBeCalledWith(subscription._token)
+  })
+})
