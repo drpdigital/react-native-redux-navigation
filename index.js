ad7dcc31f9a2617b68ee1435dc6cdc00bb0f5651
Stuart Lee <stuart.lee@drpgroup.com>
2018-03-07 14:59:43 +0000

    add localStateRouter

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export createRouter from './src/util/createRouter'
+export createLocalStateRouter from './src/util/createLocalStateRouter'
 export NavigationPropTypes from './src/components/NavigationPropTypes'
 export navigationStackReducer from './src/reducers/navigationStackReducer'
 export createReducer from './src/util/createReducer'
---
+++ src/util/createLocalStateRouter.js
@@ -0,0 +1,22 @@
+import createRouter from './createRouter'
+import navigationStackReducer from '../reducers/navigationStackReducer'
+
+/**
+ * Create a router which emulates the standard redux router, but operates within the scope of a stateful component
+ * rather than within a redux store
+ * @param resolve a resolve function
+ * @param scope the scope of the parent component, used to resolve state and setState
+ * @param key the state key where the navigation state resides
+ * @returns {{ resolve, pop, push, reset }}
+ */
+const createLocalStateRouter = (resolve, scope, key = 'navigation') => {
+  const dispatch = action => {
+    const state = scope.state[key]
+    scope.setState({
+      [key]: navigationStackReducer(state, action)
+    })
+  }
+  return createRouter(dispatch, resolve)
+}
+
+export default createLocalStateRouter
