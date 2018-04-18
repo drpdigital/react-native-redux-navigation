0786bfac078450576acdae40958aee07315648c3
Stuart Lee <stuart.lee@drpgroup.com>
2018-04-18 14:11:09 +0100

    fix style proptype

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Animated, StyleSheet } from 'react-native'
+import { Animated, StyleSheet, ViewPropTypes } from 'react-native'
 import PropTypes from 'prop-types'
 import { cardHorizontal } from '../styleInterpolation'
 import PointerEventsContainer from './PointerEventsContainer'
@@ -8,12 +8,12 @@ import PointerEventsContainer from './PointerEventsContainer'
  * Component that renders the scene as a card within the <CardStack />
  */
 class Card extends React.PureComponent {
+
   static propTypes = {
     onComponentRef: PropTypes.func.isRequired,
     pointerEvents: PropTypes.string.isRequired,
     renderScene: PropTypes.func.isRequired,
-    // TODO work out what is going on with the opacity being passed in and use PropTypes.style
-    style: PropTypes.any
+    style: ViewPropTypes.style
   }
 
   render () {
